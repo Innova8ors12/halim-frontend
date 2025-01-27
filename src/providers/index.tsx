951b4d4af8f2ef56a88c3fadceaa74270301c631
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2025-01-27 20:46:09 +0500

    fix: theme options removed from app

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
-import { ThemeProvider } from './ThemeProvider';
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return children;
 }
