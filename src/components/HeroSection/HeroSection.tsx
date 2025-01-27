221a99b916888147b6cc338b6a7922608bdbcb5d
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2025-01-28 00:00:18 +0500

    feat: hero section added on Home page

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { getImageProps } from 'next/image';
+import { ChevronRightIcon } from 'lucide-react';
+import HeroImage from '@/../public/assets/images/hero.png';
+import { Button } from '../ui/button';
+
+function getBackgroundImage(srcSet = '') {
+  const imageSet = srcSet
+    .split(', ')
+    .map((str) => {
+      const [url, dpi] = str.split(' ');
+      return `url("${url}") ${dpi}`;
+    })
+    .join(', ');
+  return `image-set(${imageSet})`;
+}
+
+export const HeroSection: FC = () => {
+  const {
+    props: { srcSet },
+  } = getImageProps({ alt: 'hero image', src: HeroImage });
+  const backgroundImage = getBackgroundImage(srcSet);
+  const style = { backgroundImage };
+
+  return (
+    <div
+      className="w-full h-screen bg-center bg-no-repeat bg-cover flex items-center justify-stretch"
+      style={style}
+    >
+      <div className="container py-20">
+        <h1 className="text-5xl md:text-6xl font-bold text-black max-w-2xl">
+          Driving Efficiency and Innovation in Industrial Engineering
+        </h1>
+        <p className="font-roboto text-lg md:text-xl text-black font-medium mt-8 max-w-md leading-6">
+          Expert consulting services to optimize your operations and achieve your goals following IE
+          standards.
+        </p>
+        <Button size={'lg'} className="mt-8">
+          Get Started <ChevronRightIcon size={24} strokeWidth={4} />
+        </Button>
+      </div>
+    </div>
+  );
+};
