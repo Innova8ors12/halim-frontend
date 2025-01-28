37670b9089b5cee6802e50693d9a5a78b5ecffd8
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2025-01-28 21:08:31 +0500

    fix: hero section changed

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -28,10 +28,10 @@ export const HeroSection: FC = () => {
       style={style}
     >
       <div className="container py-20">
-        <h1 className="font-copperplate-gothic-bold text-5xl md:text-6xl font-bold text-black max-w-2xl">
+        <h1 className="font-copperplate-gothic-bold text-5xl md:text-6xl font-bold text-white max-w-2xl">
           Driving Efficiency and Innovation in Industrial Engineering
         </h1>
-        <p className="font-copperplate-gothic-light text-lg md:text-xl text-black font-medium mt-8 max-w-md leading-6">
+        <p className="font-copperplate-gothic-light text-lg md:text-xl text-white font-medium mt-8 max-w-md leading-6">
           Expert consulting services to optimize your operations and achieve your goals following IE
           standards.
         </p>
