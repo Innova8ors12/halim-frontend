f3f53e60242e289c883a91ad2d24362d0a40a8e3
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2025-01-28 17:39:33 +0500

    feat: section heading component created

---
+++ src/components/SectionHeading/SectionHeading.tsx
@@ -0,0 +1,45 @@
+import { FC, HTMLAttributes } from 'react';
+
+interface SectionHeadingProps {
+  heading: string;
+  subHeading: string;
+  headingProps?: HTMLAttributes<HTMLHeadingElement>;
+  subHeadingProps?: HTMLAttributes<HTMLHeadingElement>;
+  highlightedSpanProps?: HTMLAttributes<HTMLSpanElement>;
+}
+
+export const SectionHeading: FC<SectionHeadingProps> = ({
+  heading,
+  subHeading,
+  headingProps,
+  subHeadingProps,
+  highlightedSpanProps,
+}) => {
+  const highlightedText = heading.slice(0, 2);
+  const normalText = heading.slice(2);
+
+  return (
+    <>
+      <h1
+        {...headingProps}
+        className={`font-roboto text-gray-500 font-medium ${headingProps?.className || ''}`}
+      >
+        {highlightedText && (
+          <span
+            {...highlightedSpanProps}
+            className={`p-1 pr-0 bg-primary-light h-8 ${highlightedSpanProps?.className || ''}`}
+          >
+            {highlightedText}
+          </span>
+        )}
+        {normalText}
+      </h1>
+      <h2
+        {...subHeadingProps}
+        className={`text-4xl md:text-5xl font-bold text-black max-w-2xl mt-6 ${subHeadingProps?.className || ''}`}
+      >
+        {subHeading}
+      </h2>
+    </>
+  );
+};
