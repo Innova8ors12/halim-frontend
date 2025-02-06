09d3092640bb2dab29e419f7b6060916e7d4b238
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2025-02-06 18:05:53 +0500

    feat: ProjectsGallery section created

--- src/app/(with-news-letter-footer)/page.tsx
+++ src/app/(with-news-letter-footer)/page.tsx
@@ -1,6 +1,7 @@
 import { AboutUsSection } from '@/components/AboutUsSection/AboutUsSection';
 import { HeroSection } from '@/components/HeroSection/HeroSection';
 import { IndustriesSection } from '@/components/IndustriesSection/IndustriesSection';
+import { ProjectsGallerySection } from '@/components/ProjectsGallerySection/ProjectsGallerySection';
 import { ServiceCategorySection } from '@/components/ServiceCategorySection/ServiceCategorySection';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <IndustriesSection />
       <ServiceCategorySection />
+      <ProjectsGallerySection />
       <AboutUsSection />
     </>
   );
---
+++ src/components/ProjectsGallerySection/ProjectsGallerySection.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import ManufacturingImage from '@/../public/assets/images/projects/manufacturing.png';
+import { SectionHeading } from '../SectionHeading/SectionHeading';
+
+export const ProjectsGallerySection = () => {
+  return (
+    <section className="w-full bg-[linear-gradient(to_bottom,theme(colors.secondary)_70%,theme(colors.gray-300)_70%)] pt-20 pb-32">
+      <div className="container grid grid-cols-1 md:grid-cols-2 gap-8 items-end">
+        <div className="w-full">
+          <SectionHeading
+            heading="Empowering Industries with Tailored Engineering Solutions"
+            subHeading="Industries We Serve"
+            headingProps={{ className: 'text-white' }}
+            subHeadingProps={{ className: 'text-white' }}
+            highlightedSpanProps={{ className: '!bg-primary' }}
+          />
+        </div>
+        <p className="font-roboto text-white font-medium leading-5">
+          Explore the various industries we serve. Our consulting expertise spans multiple sectors,
+          including manufacturing, healthcare, logistics, and more. Discover how we can address the
+          specific challenges and opportunities in your industry.
+        </p>
+      </div>
+      <div className="container mt-8">
+        <div className="relative">
+          <Image
+            src={ManufacturingImage}
+            alt="Manufacturing"
+            className="w-full rounded-bl-lg"
+            placeholder="blur"
+          />
+          <div className="absolute bg-primary max-w-sm p-8 pl-16 top-20 right-12 rounded-tr-vl">
+            <h3 className="font-bold text-3xl text-secondary relative before:content-[''] before:absolute before:w-16 before:h-1 before:bg-secondary before:top-4 before:-left-24">
+              Manufacturing Efficiency Improvement
+            </h3>
+            <p className="font-poppins text-white mt-4">
+              Implemented 5S and lean manufacturing principles for a manufacturing client, resulting
+              in a 20% increase in operational efficiency."
+            </p>
+            <h4 className="font-bold text-lg text-secondary mt-4">Key Services</h4>
+            <ul className="list-disc font-poppins text-white flex flex-col gap-1 list-inside mt-3">
+              <li>5S Implementation</li>
+              <li>Lean Manufacturing</li>
+            </ul>
+            <h4 className="font-bold text-lg text-secondary mt-4">Outcome</h4>
+            <p className="font-poppins text-white mt-2">Increased productivity and reduced waste</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
