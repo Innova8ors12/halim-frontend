3d107c846d231d06e7a720dba131421e931fc324
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2025-02-07 13:14:40 +0500

    fix: contact us footer's horizontal padding issue

--- src/components/Footer/ContactUsFooter.tsx
+++ src/components/Footer/ContactUsFooter.tsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button';
 export const ContactUsFooter: FC = () => {
   return (
     <section className="w-full bg-primary">
-      <div className="container flex flex-col lg:flex-row items-start lg:items-center justify-center gap-4 px-4 py-6">
+      <div className="container flex flex-col lg:flex-row items-start lg:items-center justify-center gap-4 py-6">
         <div className="w-full text-white text-xl font-semibold text-center lg:text-left">
           <p>Interested in seeing how we can help with your next project?</p>
           <p className="mt-1">Contact Us to discuss your needs</p>
